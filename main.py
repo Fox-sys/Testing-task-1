e69eb872bf7755de71964ea8768294b3a2f3d368
Fox-sys <59114008+Fox-sys@users.noreply.github.com>
2020-12-06 22:19:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from jsonschema import validate, ValidationError
 schemas = {} # Подготовка словоря для схем
 for i in os.listdir('schema'):
     schemas[f"{i[:-7]}"] = json.load(open(f"schema\\{i}", "r"))
-events = [json.load(open(f"event\\{i}", "r")) for i in os.listdir("event")] # создание списка с евентами
+events = [json.load(open(f"event\\{i}", "r")) for i in os.listdir("event")] # создание списка с эвентами
 
 open("logs.txt", "w")
 logs = open("logs.txt", "a")
 for i in events: # Валидация ошибок с их классификацией и логгированием
     try:
         validate(i, schemas[i["event"]])
+        logs.write("sucsess")
     except ValidationError as e:
         string = str(re.findall(r'\'[\w]+\'\s', str(e), flags=re.M|re.X)[0]) + "is required \n"
         logs.write(string)
